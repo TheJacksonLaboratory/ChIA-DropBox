75e2b9a7405eef8222913e085662c97eb80ebe86
Simon Zhongyuan Tian <20992686+tianzhongyuan@users.noreply.github.com>
2019-06-26 09:15:08 -0400

    Update MCP_RUN00_prepare_data.sh

--- MCP_RUN00_prepare_data.sh
+++ MCP_RUN00_prepare_data.sh
@@ -7,11 +7,11 @@ echo "This data is download from GEO (SRR...) or NOT? (SRR/NOT):"
 read SRR
 if [ $SRR == "SRR" ]
 then
-cp MPC.pc/PX_0201b_GEO_SC_SBAM_by_SNBC.py  MPC.pc/PX_0201_SC_SBAM_by_SNBC.py
+cp MCP.pc/PX_0201b_GEO_SC_SBAM_by_SNBC.py  MCP.pc/PX_0201_SC_SBAM_by_SNBC.py
 else
-cp MPC.pc/PX_0201a_ORI_SC_SBAM_by_SNBC.py  MPC.pc/PX_0201_SC_SBAM_by_SNBC.py
+cp MCP.pc/PX_0201a_ORI_SC_SBAM_by_SNBC.py  MCP.pc/PX_0201_SC_SBAM_by_SNBC.py
 fi 
-SHG0051H_GT18-08809_SI-GA-B5_S20_L004_R1_001.fastq.gz
+# SHG0051H_GT18-08809_SI-GA-B5_S20_L004_R1_001.fastq.gz
 
 
 LIB=$(pwd|rev|cut -d"/" -f1|rev)
